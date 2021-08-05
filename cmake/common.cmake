85088b38828f66a1358c45d4c666c79972427a84
nick-furry <nithin@nod-labs.com>
2021-08-05 22:23:48 +0000

    Add IREE dylib backend

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -19,7 +19,7 @@ option(USE_MATMUL_COMPILE "Use matmul-compile instead of mlir-opt for small and 
 # General options
 option(USE_COLUMN_MAJOR "Matrix format" OFF)
 option(ENABLE_CHECK "Enable verification by naive implementation" ON)
-set(SIZE_FILE ../../benchmark_sizes/benchmark_small_sizes.txt CACHE FILEPATH "File containing matrix sizes to be benchmarked")
+set(SIZE_FILE ../../benchmark_sizes/benchmark_all_sizes.txt CACHE FILEPATH "File containing matrix sizes to be benchmarked")
 set(TILE_FILE "" CACHE FILEPATH "File containing association between matrix size and tile size")
 set(TARGET_CPU "haswell" CACHE STRING "Target CPU for MLIR")
 set(VECTOR_WIDTH "256" CACHE STRING "Vector width for MLIR")
@@ -42,7 +42,8 @@ endif()
 # Set IREE backends to use
 if(${USE_IREE} STREQUAL "ON")
   option(IREE_VMVX "Enable IREE vmvx (CPU) backend" ON)
-  option(IREE_CUDA "Enable IREE cuda backend" ON)
+  option(IREE_DYLIB "Enable IREE dylib (CPU) backend" ON)
+  option(IREE_CUDA "Enable IREE cuda (GPU) backend" ON)
 endif()
 
 set(VARS_TO_COPY
