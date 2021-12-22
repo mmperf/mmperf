40a0c54aedc604eb93b45e032be60d74bc4a45bf
yzhang93 <zhyuhang88@gmail.com>
2021-12-22 20:16:29 +0000

    Update iree_sandbox_matmul.py to use benchmark experts
    To test:
    cmake -GNinja -DCMAKE_CXX_COMPILER=clang++-11 -DCMAKE_C_COMPILER=clang-11 -DUSE_IREE_LLVM_SANDBOX=ON -B build .
    cmake --build build
    python mmperf.py ./build/matmul results -sandbox -matrix_sizes=/path/to/size_list

--- iree_sandbox_matmul.py
+++ iree_sandbox_matmul.py
@@ -14,153 +14,30 @@ from pathlib import Path
 ### Compilation strategies.
 ################################################################################
 
-
-def TestExpert(transforms: tp.Sequence[tp.Union[Transform,
-                                                TransformationList]]):
-  return (TransformationList(transforms=transforms) + Bufferize() +
-          LoweringOnlyExpert('matmul_on_tensors', 'linalg.generic'))
-
-# TODO: Check generate code for basic code quality, e.g., no linalg.copy.
-
-# No tiling.
-expert_no_tiling = LoweringOnlyExpert('matmul_on_tensors', 'linalg.generic')
-
-# 1 level of tiling.
-expert_tile_1 = SingleTilingExpert('matmul_on_tensors',
-                                   'linalg.generic',
-                                   tile_sizes=[8, 8, 24],
-                                   pad=False,
-                                   peel=[])
-
-# 1 level of tile and interchange.
-expert_tile_and_interchange_1 = SingleTilingExpert('matmul_on_tensors',
-                                                   'linalg.generic',
-                                                   tile_sizes=[8, 8, 24],
-                                                   tile_interchange=[2, 0, 1],
-                                                   pad=False,
-                                                   peel=[])
-
-# 1 level of tiling and then generalize and interchange.
-expert_tile_1_and_generalize_interchange = \
-    Tile.then(Generalize).then(Vectorize).then(LoweringOnlyExpert)(\
-      'matmul_on_tensors',                                         \
-      'linalg.generic',                                            \
-      tile_sizes=[8, 8, 24],                                       \
-      tile_interchange=[2, 0, 1],                                  \
-      iterator_interchange=[0, 2, 1])
-
-# 1 level of tiling, peel, scalarize the remaining dynamic dims.
-# TODO: scalarize_dyn_dims should be exposed as a variable in Tile transformation
-# to enable tuning and pass it into the transformation list directly.
-expert_tile_1_peel_scalarize = TransformationList(
-    transforms=[
-        Tile('matmul_on_tensors',
-             'linalg.generic',
-             tile_sizes=[8],
-             peel=[0]),
-        Tile('matmul_on_tensors', 'linalg.generic', scalarize_dyn_dims=True),
-    ] + Vectorize.then(LoweringOnlyExpert)
-    ('matmul_on_tensors', 'linalg.generic').transforms)
-
-# 1 level of tiling, with padding.
-expert_tile_1_pad = Tile(
-    'matmul_on_tensors',
-    'linalg.generic',
-    tile_sizes=[8, 8, 24],
-    pad=True,
-    pack_paddings=[1, 1, 1]).then(
-        Vectorize('matmul_on_tensors', 'linalg.generic') +
-        LoweringOnlyExpert('matmul_on_tensors', 'linalg.generic'))
-
-# 1 level of tiling, with padding, hoisted.
-expert_tile_1_pad_hoist = TestExpert([
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[8, 8, 64],
-         pad=True,
-         pack_paddings=[1, 1, 1],
-         hoist_paddings=[3, 3, 3]),
-    Vectorize('matmul_on_tensors', 'linalg.generic')
-])
-# 2 levels of tiling, with padding, hoisted.
-expert_tile_2_pad_hoist = TestExpert([
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[8, 8, 24]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[4, 4, 12],
-         pad=True,
-         pack_paddings=[1, 1, 1],
-         hoist_paddings=[6, 6, 6]),
-    Vectorize('matmul_on_tensors', 'linalg.generic')
-])
-# 3 levels of tiling, with padding, hoisted. Peeling on the 3rd level.
-expert_tile_3_pad_hoist_peel = TestExpert([
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[8, 8, 24]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[4, 4, 12],
-         pad=True,
-         pack_paddings=[1, 1, 1],
-         hoist_paddings=[6, 6, 6]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[2, 3, 7],
-         peel=[0, 1, 2]),
-    Vectorize('matmul_on_tensors', 'linalg.generic')
-])
-# 3 levels of tiling, with padding, hoisted. Peeling on the 3rd level.
-# Scalarize remaining dynamic dims.
-expert_tile_3_pad_hoist_peel_scalarize = TestExpert([
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[8, 8, 24]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[4, 4, 12],
-         pad=True,
-         pack_paddings=[1, 1, 1],
-         hoist_paddings=[6, 6, 6]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[2, 3, 7],
-         peel=[0, 1, 2]),
-    Tile('matmul_on_tensors', 'linalg.generic', scalarize_dyn_dims=True),
-    Vectorize('matmul_on_tensors', 'linalg.generic')
-])
-# Fuse, then tile.
-expert_fuse_2_tile_1 = TestExpert([
-    Fuse('matmul_on_tensors', 'linalg.generic', tile_sizes=[8, 16, 0]),
-    Fuse('matmul_on_tensors', 'linalg.generic', tile_sizes=[4, 4, 0]),
-    Tile('matmul_on_tensors', 'linalg.generic', tile_sizes=[0, 0, 24]),
-    Vectorize('matmul_on_tensors', 'linalg.generic'),
-    Vectorize('matmul_on_tensors', 'linalg.fill')
-])
-expert_fuse_and_pad = TestExpert([
-    Fuse('matmul_on_tensors', 'linalg.generic', tile_sizes=[16, 16, 0]),
-    Tile('matmul_on_tensors',
-         'linalg.generic',
-         tile_sizes=[8, 8, 32],
-         pad=True,
-         pack_paddings=[1, 1, 1],
-         hoist_paddings=[3, 3, 3]),
-    Vectorize('matmul_on_tensors', 'linalg.generic'),
-    Tile('matmul_on_tensors', 'linalg.fill', tile_sizes=[8, 8]),
-    Vectorize('matmul_on_tensors', 'linalg.fill')
-])
-
 all_experts = [
-    e.print_ir(after_all=False) for e in [
-        expert_no_tiling, expert_tile_1, expert_tile_and_interchange_1,
-        expert_tile_1_and_generalize_interchange, expert_tile_1_peel_scalarize,
-        expert_tile_1_pad, expert_tile_1_pad_hoist, expert_tile_2_pad_hoist,
-        expert_tile_3_pad_hoist_peel, expert_tile_3_pad_hoist_peel_scalarize,
-        expert_fuse_2_tile_1, expert_fuse_and_pad
-    ]
-]
+    e.print_ir(after_all=False, at_begin=False, llvm=False) for e in [
+    SingleTilingExpert('matmul_on_tensors',
+                       'linalg.generic',
+                       tile_sizes=[12, 32, 8],
+                       tile_interchange=[0, 1, 2],
+                       pad=True,
+                       pack_paddings=[1, 1, 0],
+                       hoist_paddings=[2, 3, 0]),
+    DoubleTileAndDecompose('matmul_on_tensors',
+                            'linalg.generic',
+                            tile_sizes1=[288, 128, 512],
+                            tile_interchange1=[0, 2, 1],
+                            tile_sizes2=[9, 32, 16],
+                            tile_interchange2=[0, 1, 2],
+                            pad2=True,
+                            pack_paddings2=[1, 1, 0],
+                            hoist_paddings2=[5, 6, 0]).then(\
+      Vectorize('matmul_on_tensors',
+                'linalg.generic')).then(\
+      LoweringOnlyExpert('matmul_on_tensors',
+                         'linalg.generic',
+                         transpose_lowering='eltwise'))
+    ]]
 
 ################################################################################
 ### Problem instantiations.
@@ -168,8 +45,7 @@ all_experts = [
 
 keys = ['m', 'n', 'k']
 
-
-def make_size_list(sizes: Sequence[int]):
+def make_size_list(sizes: Sequence):
   return {k: v for k, v in zip(keys, sizes)}
 
 def path_expand(s):
@@ -183,11 +59,7 @@ def main(argv):
   args = parser.parse_args(argv[1:])
 
   n_iters = 100
-  expert_lists = ["expert_no_tiling", "expert_tile_1", "expert_tile_and_interchange_1",
-                  "expert_tile_1_and_generalize_interchange", "expert_tile_1_peel_scalarize",
-                  "expert_tile_1_pad", "expert_tile_1_pad_hoist", "expert_tile_2_pad_hoist",
-                  "expert_tile_3_pad_hoist_peel", "expert_tile_3_pad_hoist_peel_scalarize",
-                  "expert_fuse_2_tile_1", "expert_fuse_and_pad"]
+  expert_lists = ["single_tiling_expert", "double_tile_and_decompose"]
 
   with open(args.matrix_path, 'r') as f:
     all_sizes = f.readlines()
