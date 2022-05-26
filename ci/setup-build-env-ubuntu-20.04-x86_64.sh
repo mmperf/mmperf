d74e21e380973dc7b7f3738955de984046a55c54
yzhang93 <zhyuhang88@gmail.com>
2022-05-26 17:46:15 +0000

    Update CMake version to align with IREE

--- ci/setup-build-env-ubuntu-20.04-x86_64.sh
+++ ci/setup-build-env-ubuntu-20.04-x86_64.sh
@@ -20,7 +20,7 @@ python3.9 -m venv .
 source bin/activate
 pip install -r "$SCRIPT_DIR/../requirements.txt"
 
-wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-linux-x86_64.sh \
-  -O /tmp/cmake-3.18.4-linux-x86_64.sh
-bash /tmp/cmake-3.18.4-linux-x86_64.sh --prefix=. --exclude-subdir --skip-license
-rm /tmp/cmake-3.18.4-linux-x86_64.sh
+wget https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.sh \
+  -O /tmp/cmake-3.22.2-linux-x86_64.sh
+bash /tmp/cmake-3.22.2-linux-x86_64.sh --prefix=. --exclude-subdir --skip-license
+rm /tmp/cmake-3.22.2-linux-x86_64.sh
