5fa0ce48d2b93baa665d682ec8d5ff425615e4c0
Harsh <harsh@nod.com>
2022-08-12 15:57:33 -0700

    Allocate tensor shapes on the heap instead of the stack
    This fixes issues running miniLM sizes on
    Intel Xeons.

--- matmul-iree/src/device_dylib.c
+++ matmul-iree/src/device_dylib.c
@@ -26,7 +26,7 @@ iree_status_t create_sample_device(iree_allocator_t host_allocator,
       iree_hal_executable_import_provider_null(), host_allocator, &loader));
 
   // Use the default host allocator for buffer allocations.
-  iree_string_view_t identifier = iree_make_cstring_view("dylib");
+  iree_string_view_t identifier = iree_make_cstring_view("local-sync");
   iree_hal_allocator_t* device_allocator = NULL;
   iree_status_t status = iree_hal_allocator_create_heap(
       identifier, host_allocator, host_allocator, &device_allocator);
